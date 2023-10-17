55145ad766164e9dfd61e393d7f05611473f2a5d
kayle2915 <144207967+kayle2915@users.noreply.github.com>
2023-10-17 09:51:53 +0400

    exercise_1.py

--- Chapter 4- Control Flow/Exercises/exercise_1.py
+++ Chapter 4- Control Flow/Exercises/exercise_1.py
@@ -1,5 +1,15 @@
 alien_color = 'green'
 
 if alien_color == 'green':
-    print("You just earned 5 points!")
+    print("You just earned 5 points!") 
+
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print("you just earned 5 points")
+
+
+
+
 
