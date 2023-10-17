9f3ed66238bbea90644c4cc6004217b3904cb820
kayle2915 <144207967+kayle2915@users.noreply.github.com>
2023-10-17 09:53:15 +0400

    execise2.py

---
+++ Chapter 4- Control Flow/Exercises/exercise2.py
@@ -0,0 +1,14 @@
+alien_color = input("Enter a color: ")
+
+if alien_color == "green":
+    print("you just earned 5 points for shooting an alien ")
+elif alien_color == "red":
+    print("you just earn 10 points")
+else:
+    print("invalid output")
+
+
+
+
+
+
