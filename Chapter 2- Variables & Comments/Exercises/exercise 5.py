2645d6f43d7bd59f99a6fe68f36c1bfa3139dd24
kayle2915 <144207967+kayle2915@users.noreply.github.com>
2023-10-30 22:08:25 +0400

    Update exercise 5.py

--- Chapter 2- Variables & Comments/Exercises/exercise 5.py
+++ Chapter 2- Variables & Comments/Exercises/exercise 5.py
@@ -4,9 +4,9 @@ USB_per_stick = 6
 
 #total of USB she can buy
 
-Total_USB = (50  / 6 )
+Total_USB = (50/6)
 
-print(f"She can buy {USB_per_stick} USB sticks.")
+print(f"She can buy {Total_USB} USB sticks.")
 
 #how many pound she has left 
 
