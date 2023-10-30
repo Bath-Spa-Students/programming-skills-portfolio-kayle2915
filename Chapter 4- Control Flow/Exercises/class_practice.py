a1a1441dcb0870245518835f7253b04cc37e917d
kayle2915 <144207967+kayle2915@users.noreply.github.com>
2023-10-30 22:06:41 +0400

    Create class_practice.py

---
+++ Chapter 4- Control Flow/Exercises/class_practice.py
@@ -0,0 +1,12 @@
+# cars to buy according to money
+
+salary = float(input("enter your salary : "))
+cars = 100000
+
+if salary > 100000:
+    print("new lamborghini")
+
+else: 
+   print("cannot buy new one")
+
+
