503b6b001620fbc9b964559327f7375b1cef90f4
kayle2915 <144207967+kayle2915@users.noreply.github.com>
2023-11-03 15:54:33 +0400

    Create exercise3.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise3.py
@@ -0,0 +1,21 @@
+# Glosarry
+
+glossary = {
+    'list'  : 'A data type that can store a collection of values',
+    'statement' : 'a line of code that performs a specific task',
+    'variable' : 'use to store data',
+    'function' : ' a block of code that performs a specific task',
+    'argument' : ' a value that is given to a function when it is called',
+
+}
+
+glossary.update({
+    'elif' : 'else if',
+    'Program': 'a set of instruction that a computer follows',
+    'float': 'uses to convert a number that contains decimal',
+    'int': 'use to represent an integer value.'
+})
+
+ 
+for word , meaning in glossary.items():
+ print(f"{word}: {meaning}\n")
