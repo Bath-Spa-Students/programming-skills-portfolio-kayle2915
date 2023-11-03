f02e6b13ba56187fdb43d201ee9493582029f426
kayle2915 <144207967+kayle2915@users.noreply.github.com>
2023-11-03 15:54:13 +0400

    Create exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/exercise1.py
@@ -0,0 +1,17 @@
+# Persons information
+
+person = {
+    'first_name' : 'kayle',
+    'second_name' : 'chester',
+    'last_name' : 'abenojar',
+    'age' : '19',
+    'city' : 'International city Dubai',
+}
+
+# printing persons information
+
+print(person['first_name'])
+print(person['second_name'])
+print(person['last_name'])
+print(person['age'])
+print(person['city'])
