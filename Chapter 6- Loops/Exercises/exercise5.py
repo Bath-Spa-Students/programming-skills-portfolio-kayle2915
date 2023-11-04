3e94855afdb718d6b912dbff77edb4cb43a5215b
kayle2915 <144207967+kayle2915@users.noreply.github.com>
2023-11-04 23:57:20 +0400

    Update exercise5.py

--- Chapter 6- Loops/Exercises/exercise5.py
+++ Chapter 6- Loops/Exercises/exercise5.py
@@ -1,12 +1,13 @@
+# list of sandwiches
 sandwich_orders = ["pastrami", "tuna", "steak"]
 finished_sandwiches = []
 
-
+# printing we've run out of pastrami
 while 'pastrami' in sandwich_orders:
      print("I'm sorry, we're all out of pastrami today.")
      sandwich_orders.remove("pastrami")
 
-
+# loops
 while True:
     order = input("Enter your sandwich (type 'exit' to finish): ")
     if order.lower() == 'exit':
@@ -17,6 +18,7 @@ while True:
     print(f"I made your {order} sandwich.")
     finished_sandwiches.append(order)
 
+# printing the finished sandwiches
 print("\nList of finished sandwiches:")
 for sandwich in finished_sandwiches:
     print(sandwich)
