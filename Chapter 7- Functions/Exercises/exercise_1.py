1c9cdecb0bd35b43841d4d299187a2a316cce90f
kayle2915 <144207967+kayle2915@users.noreply.github.com>
2023-11-04 23:55:34 +0400

    Create exercise_1.py

---
+++ Chapter 7- Functions/Exercises/exercise_1.py
@@ -0,0 +1,9 @@
+# exercise 1 (functions)
+def display_message():
+    """Display a message about what I'm learning in this chapter."""
+    print("In this chapter, I am learning about functions in Python.")
+
+
+
+# Call the function to display the message
+display_message()
