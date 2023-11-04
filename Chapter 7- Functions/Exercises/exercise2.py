2c9983712616a1bc31f681b09f8f98042859a1e4
kayle2915 <144207967+kayle2915@users.noreply.github.com>
2023-11-04 23:55:13 +0400

    Create exercise2.py

---
+++ Chapter 7- Functions/Exercises/exercise2.py
@@ -0,0 +1,9 @@
+# Favorite book
+
+def favorite_book(title):
+    """Display a message about my favorite book"""
+    print(f"{title} is one of my favorite book of all time")
+
+
+# name of the book 
+favorite_book('The young elites')
